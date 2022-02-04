70c083f4c848550f4182395120d95a5ce5f1e2cd
eric bel <ericbelecov@gmail.com>
2022-02-04 07:28:14 +0400

    using i18n

--- src/components/Header/HeaderTop/LanguagesComp/LanguagesComp.jsx
+++ src/components/Header/HeaderTop/LanguagesComp/LanguagesComp.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { withTranslation } from "react-i18next";
+// import { useTranslation } from "react-i18next";
 
-const LanguagesComp = () => {
-  return <div>English</div>;
+const LanguagesComp = ({ t, i18n }) => {
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
+  return (
+    <div className="mt-10">
+      <button onClick={() => changeLanguage("en")}>EN</button>
+      <button onClick={() => changeLanguage("ru")}>RU</button>
+      <button onClick={() => changeLanguage("en")}>EN</button>
+      <hr />
+      <div>
+        <h1>{t("title")}</h1>
+      </div>
+      <div>{t("description.part1")}</div>
+      <div>{t("description.part2")}</div>
+    </div>
+  );
 };
 
-export default LanguagesComp;
+export default withTranslation()(LanguagesComp);
