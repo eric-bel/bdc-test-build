5cfae7686d0b740c0b66546b8270766a68759173
eric bel <ericbelecov@gmail.com>
2022-02-06 00:36:44 +0400

    antd fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div>
       <HeaderTop />
-      {/* <HeaderBottom /> */}
+      {/* <HeaderBottom /> */} 
     </div>
   );
 };
--- src/components/Header/HeaderTop/LanguagesComp/LanguagesComp.jsx
+++ src/components/Header/HeaderTop/LanguagesComp/LanguagesComp.jsx
@@ -1,26 +1,40 @@
 import React from "react";
-
-// const LanguagesComp = () => {
-//   console.log(React);
-//   return <div className="">English</div>;
-// };
-
-// export default LanguagesComp;
-
 import { withTranslation } from "react-i18next";
+import { Menu, Dropdown } from "antd";
+import { DownOutlined } from "@ant-design/icons";
 
-const LanguagesComp = ({ t, i18n }) => {
+const LanguagesComp = ({ i18n }) => {
   const changeLanguage = (language) => {
     i18n.changeLanguage(language);
   };
+  const menu = (
+    <Menu className="bg-[#091D3E]">
+      <Menu.Item key="0">
+        <button onClick={() => changeLanguage("en")}>EN</button>
+      </Menu.Item>
+      <Menu.Item key="1">
+        <button onClick={() => changeLanguage("ru")}>RU</button>
+      </Menu.Item>
+      <Menu.Item key="3">
+        <button onClick={() => changeLanguage("ge")}>ქარ</button>
+      </Menu.Item>
+    </Menu>
+  );
+
   return (
-    <div className="flex gap-4">
-      <button onClick={() => changeLanguage("en")}>EN</button>
-      <button onClick={() => changeLanguage("ru")}>RU</button>
-      <button onClick={() => changeLanguage("ge")}>ქარ</button>
-    </div>
+    // <div className="flex gap-4">
+    //   <button onClick={() => changeLanguage("en")}>EN</button>
+    //   <button onClick={() => changeLanguage("ru")}>RU</button>
+    //   <button onClick={() => changeLanguage("ge")}>ქარ</button>
+    // </div>
+
+    // onClick={(e) => e.preventDefault()}
+    <Dropdown overlay={menu} trigger={["click"]} onClick={(e) => e.preventDefault()}>
+      <a href="/" className="ant-dropdown-link">
+        EN RU ქარ <DownOutlined />
+      </a>
+    </Dropdown>
   );
 };
 
 export default withTranslation()(LanguagesComp);
-
