b37228e1915d83df42e770cc5c972215b92bc9e3
eric bel <ericbelecov@gmail.com>
2022-02-08 08:30:50 +0400

    making carousel

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
 // import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-// import Main from "./components/Main/Main";
+import Main from "./components/Main/Main";
+
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
       <div className="h-screen bg-slate-500">
         <div className="h-screen main_container bg-slate-700"></div>
       </div>
-      {/* <Main /> */}
+      
       {/* <Footer /> */}
     </>
   );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
+import Carousel from "./Carousel/Carousel";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div>
+      <Carousel />
+    </div>
+  );
 };
 
 export default Main;
