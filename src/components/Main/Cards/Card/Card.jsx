a437623109f1e79d5a9567d092eb1e9f15c63563
eric bel <ericbelecov@gmail.com>
2022-02-12 02:00:03 +0400

    cards

--- src/components/Main/Cards/Card/Card.jsx
+++ src/components/Main/Cards/Card/Card.jsx
@@ -1,11 +1,21 @@
 import React from "react";
-import styles from "./Card.module.css";
+import s from "./Card.module.css";
 
-const Card = () => {
+const Card = ({ img, title, text }) => {
+  // rounded-bl-[200px, 23px] flex flex-col h-[300px] w-[350px]
+  // bg-white shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]
+  // mb-[100px] mt-4
   return (
-    <div className={styles.card}>
-      {" "}
-      <div></div>
+    <div className={s.card}>
+      <img className="flex items-center w-20" src={img} alt="icons"></img>
+      <div
+        className="text-center font-semibold text-[#091D3E]
+      desc:text-[16px]
+      "
+      >
+        {title}
+      </div>
+      <div className="w-3/4 text-center font-normal text-slate-500">{text}</div>
     </div>
   );
 };
--- src/components/Main/Cards/Cards.jsx
+++ src/components/Main/Cards/Cards.jsx
@@ -1,11 +1,39 @@
 import React from "react";
 import Card from "./Card/Card";
+import cos from "../../../assets/serv.png";
+import implant from "../../../assets/implant.png";
+import care from "../../../assets/dentistry.png";
 
 const Cards = () => {
   return (
-    <div className="main_container">
-      <Card />
-      
+    <div
+      className="main_container flex gap-10 justify-center
+    mob:flex-col mob:gap-y-1 mob:items-center
+    tab:flex-col tab:gap-y-1 tab:items-center
+    md:flex-col md:gap-y-1 md:items-center
+    "
+    >
+      <Card
+        img={cos}
+        title={"Косметическая стоматология"}
+        text={
+          "Косметическая стоматология обычно используется для обозначения любой стоматологической работы, которая улучшает внешний вид зубов, десен или прикуса."
+        }
+      />
+      <Card
+        img={implant}
+        title={"Зубные имлпанты"}
+        text={
+          "Зубные имплантаты представляют собой хирургические приспособления, вживляемые в челюстную кость, которые затем срастаются с челюстной костью в течение нескольких месяцев.."
+        }
+      />
+      <Card
+        img={care}
+        title={"Общая стоматологическая помощь"}
+        text={
+          "Уделяя большое внимание здоровью полости рта и гигиене, стоматологи общего профиля помогают людям избежать прогрессирования заболевания."
+        }
+      />
     </div>
   );
 };
