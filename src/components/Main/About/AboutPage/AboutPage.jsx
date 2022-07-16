4e077eb8741541e08590b687171182cfd64ec4ab
eric bel <ericbelecov@gmail.com>
2022-07-16 18:50:24 +0400

    logo and text done

--- src/components/Main/About/AboutPage/AboutPage.jsx
+++ src/components/Main/About/AboutPage/AboutPage.jsx
@@ -2,14 +2,19 @@ import { withTranslation } from "react-i18next";
 import About from "../About";
 import teamParty from "../../../../assets/aboutImg/teamParty.jpg";
 import AboutMore from "./AboutMore/AboutMore";
-import logo from "../../../../assets/logo.png";
+import logo from "../../../../assets/newlogo1.jpeg";
 
 // { t }
 
 const AboutPage = () => {
   return (
     <div className="mb-10">
-      <img src={logo} alt="about us logo" className="w-1/2 mt-10"></img>
+      <img
+        src={logo}
+        alt="about us logo"
+        className="w-3/5 mt-10 mx-auto"
+      ></img>
+      <div className="italic text-[52px] mt-10 text-center">All kinds of dental services</div>
       <About img={teamParty} />
       <AboutMore />
     </div>
