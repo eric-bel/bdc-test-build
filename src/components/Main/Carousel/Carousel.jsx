a3f872e109d9abd0e1786461e771cf6086e0e9b7
eric bel <ericbelecov@gmail.com>
2022-07-21 15:40:38 +0400

    green color text and tex correction about teeth channels

--- src/components/Main/Carousel/Carousel.jsx
+++ src/components/Main/Carousel/Carousel.jsx
@@ -7,12 +7,12 @@ const Carousel = () => {
       <img className="w-full" src={mainImg} alt="mainImg"></img>
 
       <h1
-        className="absolute font-semibold text-sky-500
+        className="absolute font-semibold text-[#16a34a]
         mob:top-[35px] mob:left-[35%] mob:text-[8px]
         tab:top-[75px] tab:left-[35%] tab:text-[12px]
         md:top-[103px] md:left-[35%] md:text-[15px]
         lap:top-[143px] lap:left-[40%] lap:text-[15px]
-        desc:top-[120px] desc:left-[43.6%] desc:text-[20px]
+        desc:top-[10%] desc:left-[43.6%] desc:text-[20px]
         xdesc:top-[140px] xdesc:left-[41%] xdesc:text-[25px]
         "
       >
@@ -24,7 +24,7 @@ const Carousel = () => {
         tab:top-[95px] tab:left-[35%] tab:text-[22px] tab:leading-[20px]
         md:top-[135px] md:left-[35%] md:text-[35px] md:leading-[34px]
         lap:top-[180px] lap:left-[40%] lap:text-[40px] lap:leading-[40px]
-        desc:top-[160px] desc:left-[43%] desc:text-[55px] desc:leading-[60px]
+        desc:top-[15%] desc:left-[43%] desc:text-[55px] desc:leading-[60px]
         xdesc:top-[190px] xdesc:left-[41%] xdesc:text-[75px] xdesc:leading-[75px]
         "
       >
@@ -36,7 +36,7 @@ const Carousel = () => {
         tab:top-[165px] tab:left-[35%] tab:text-[12px] tab:leading-[12px]
         md:top-[248px] md:left-[35%] md:text-[13px] md:leading-[15px]
         lap:top-[320px] lap:left-[40%] lap:text-[15px] lap:leading-[17px]
-        desc:top-[360px] desc:left-[43%] desc:text-[17px]
+        desc:top-[40%] desc:left-[43%] desc:text-[17px]
         xdesc:top-[440px] xdesc:left-[41%] xdesc:text-[17px]
       "
       >
